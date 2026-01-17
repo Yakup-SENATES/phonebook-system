8918aee39a6e1125f650c1c376082066e5ba8fb8
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2026-01-18 00:27:46 +0300

    feat(contact-service): add mapper for contactInfo and person

---
+++ contact-service/src/main/java/com/phonebook_system/contact_service/mapper/ContactInfoMapper.java
@@ -0,0 +1,21 @@
+package com.phonebook_system.contact_service.mapper;
+
+import com.phonebook_system.contact_service.entity.ContactInfoEntity;
+import com.phonebook_system.contact_service.model.request.CreateContactInfoRequest;
+import com.phonebook_system.contact_service.model.response.ContactInfoResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ContactInfoMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "person", ignore = true)
+    ContactInfoEntity toEntity(CreateContactInfoRequest request);
+
+    ContactInfoResponse toResponse(ContactInfoEntity entity);
+
+    List<ContactInfoResponse> toResponseList(List<ContactInfoEntity> entities);
+}
---
+++ contact-service/src/main/java/com/phonebook_system/contact_service/mapper/PersonMapper.java
@@ -0,0 +1,29 @@
+package com.phonebook_system.contact_service.mapper;
+
+import com.phonebook_system.contact_service.entity.PersonEntity;
+import com.phonebook_system.contact_service.model.request.CreatePersonRequest;
+import com.phonebook_system.contact_service.model.request.UpdatePersonRequest;
+import com.phonebook_system.contact_service.model.response.PersonDetailResponse;
+import com.phonebook_system.contact_service.model.response.PersonResponse;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = { ContactInfoMapper.class })
+public interface PersonMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "contactInfoList", ignore = true)
+    PersonEntity toEntity(CreatePersonRequest request);
+
+    PersonResponse toResponse(PersonEntity entity);
+
+    PersonDetailResponse toDetailResponse(PersonEntity entity);
+
+    List<PersonResponse> toResponseList(List<PersonEntity> entities);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "contactInfoList", ignore = true)
+    void updateEntityFromRequest(UpdatePersonRequest request, @MappingTarget PersonEntity entity);
+}
