e4be68e732996ab4e6eccde6c1c8d72c483839f0
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2026-01-18 14:41:21 +0300

    feat(report-service): create ReportRequestEvent for kafka events

---
+++ report-service/src/main/java/com/phonebook_system/report_service/model/event/ReportRequestEvent.java
@@ -0,0 +1,18 @@
+package com.phonebook_system.report_service.model.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReportRequestEvent {
+    private UUID reportId;
+    private LocalDateTime requestDate;
+}
