593dc6959031a16d32e4a6fa289c2318664276a9
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2026-01-18 17:14:11 +0300

    feat(report-service): add findByStatusNotCompleted in ReportRepository

--- report-service/src/main/java/com/phonebook_system/report_service/repository/ReportRepository.java
+++ report-service/src/main/java/com/phonebook_system/report_service/repository/ReportRepository.java
@@ -2,10 +2,16 @@ package com.phonebook_system.report_service.repository;
 
 import com.phonebook_system.report_service.entity.ReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ReportRepository extends JpaRepository<ReportEntity, UUID> {
+
+
+    @Query("SELECT r FROM ReportEntity r WHERE r.status <> 'COMPLETED'")
+    Optional<ReportEntity> findByStatusNotCompleted();
 }
