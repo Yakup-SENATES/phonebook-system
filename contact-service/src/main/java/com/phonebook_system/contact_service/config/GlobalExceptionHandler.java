3ffb0c5b65d2698c73dcd4a3affc8a40b6f41b5d
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2026-01-20 00:26:19 +0300

    feat(contact-service): add empty persons check and custom exception handling

--- contact-service/src/main/java/com/phonebook_system/contact_service/config/GlobalExceptionHandler.java
+++ contact-service/src/main/java/com/phonebook_system/contact_service/config/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.phonebook_system.contact_service.config;
 
 import com.phonebook_system.contact_service.base.ErrorResponse;
-import com.phonebook_system.contact_service.model.exception.ContactInfoNotFoundException;
-import com.phonebook_system.contact_service.model.exception.InvalidContactInfoException;
-import com.phonebook_system.contact_service.model.exception.PersonNotFoundException;
+import com.phonebook_system.contact_service.model.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +15,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PersonNotFoundException.class)
     public ResponseEntity<ErrorResponse> handlePersonNotFound(
             PersonNotFoundException ex) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(
+                        ex.getMessage(),
+                        HttpStatus.NOT_FOUND.value(),
+                        Instant.now()
+                ));
+    }
+    @ExceptionHandler(PersonsNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePersonsNotFound(
+            PersonsNotFoundException ex) {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(
@@ -45,6 +54,18 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse(
                         ex.getMessage(),
                         HttpStatus.BAD_REQUEST.value(),
+                        Instant.now()
+                ));
+    }
+
+    @ExceptionHandler(LocationStatsNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleLocationStatsNotFound(
+            LocationStatsNotFoundException ex) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(
+                        ex.getMessage(),
+                        HttpStatus.NOT_FOUND.value(),
                         Instant.now()
                 ));
     }
